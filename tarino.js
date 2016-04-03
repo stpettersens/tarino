4c49b79bd7d4b4d3c4bc1f601e250ace0fbd4d63
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-04-03 21:17:27 +0100

    gp; can tar more than one file.

--- tarino.js
+++ tarino.js
@@ -9,9 +9,11 @@
 'use strict'
 
 const fs = require('fs')
+const path = require('path')
 const zlib = require('zlib')
 
 const NC = String.fromCharCode(0)
+const PADDING = 1029
 
 function decToPaddedOctal (num, length) {
   let octal = parseInt(num, 10).toString(8)
@@ -38,49 +40,104 @@ function calcChecksum (header) {
   return decToPaddedOctal(checksum - 64, 6)
 }
 
-module.exports.createTar = function (tarname, filename, options) {
+function getContents (srcpath) {
+  return fs.readdirSync(srcpath)
+}
+
+function writeTarEntry (tarname, filename, callback) {
   let contents = fs.readFileSync(filename, 'ascii').toString()
   let stats = fs.statSync(filename)
   let size = decToPaddedOctal(stats['size'], 11)
   let modified = decToPaddedOctal(Date.parse(stats['mtime']) / 1000, 0)
-  let tar = null;
+  let type = 0
+  /**
+    * TAR FORMAT SPECIFICATION
+    * (a) File name (0-)
+    * (b) File mode (100; 8)
+    * (c) Owner's numeric user ID (108; 8)
+    * (d) Group's numeric user ID (116; 8)
+    * (e) File size in bytes (octal) (124; 12)
+    * (f) Last modification time in numeric Unix time format (octal) (136; 12)
+    * (g) Checksum for header record (148; 8)
+    * (h) Link indicator (file type) (156; 1)
+    * (i) UStar indicator (257; 6)
+  */
+  let tar = fs.createWriteStream(tarname, {start: 0, flags: 'w'})
+  let header = `${filename}${padData(101 - filename.length)}` // (a)
+  header += `0100777${NC}0000000${NC}0000000${NC}${size}${NC}${modified}${NC}` // (b, c, d, e, f)
+  header += `000000${NC} ${type}${padData(257-156)}ustar${NC}00${padData(512-264)}` // (g, h, i)
+  let data = `${contents}${padData(320)}` // !!! was 512, 323, 238, 512
+  tar.write(header + data)
+  tar.close()
+  callback(header)
+}
 
-  if(options.flat) {
+function writeChecksum (tarname, header, callback) {
+  let tar = fs.createWriteStream(tarname, {start: 148, flags: 'r+'})
+  tar.write(calcChecksum(header))
+  tar.close()
+  fs.readFile(tarname, function(err, data) {
+    return callback(data)
+  })
+}
+
+function writeTarEntries (tarname, entries, callback) {
+  for(let i = 0; i < entries.length; i++) {
+    writeTarEntry(entries[i].part, entries[i].file, function (header) {
+      writeChecksum(entries[i].part, header, function(data) {
+        fs.appendFileSync(tarname, data.toString())
+        if (i === entries.length - 1) {
+          fs.appendFileSync(tarname, padData(PADDING))
+        }
+      })
+    })
+  }
+  entries.map(function (entry) {
+    fs.unlinkSync(entry.part)
+  })
+}
+
+module.exports.createTar = function (tarname, filename, options) {
+  if (options && options.flat) {
     let fns = filename.split(/\//)
     filename = fns[fns.length - 1]
   }
 
+  if (options && options.root) {
+    console.log(options.root)
+  }
+
+  if (options && options.folder) {
+    console.log(getContents(options.root))
+  }
+
   try {
-    tar = fs.createWriteStream(tarname, {flags: 'w'})
-    if (filename.length < 100) {
-     /** 
-      * TAR FORMAT SPECIFICATION
-      * (a) File name (0-) 
-      * (b) File mode (100; 8)
-      * (c) Owner's numeric user ID (108; 8) 
-      * (d) Group's numeric user ID (116; 8)
-      * (e) File size in bytes (octal) (124; 12)
-      * (f) Last modification time in numeric Unix time format (octal) (136; 12)
-      * (g) Checksum for header record (148; 8)
-      * (h) Link indicator (file type) (156; 1)
-      * (i) UStar indicator (257; 6)
-     */
-      let header = `${filename}${padData(101 - filename.length)}` // (a)
-      header += `0100777${NC}0000000${NC}0000000${NC}${size}${NC}${modified}${NC}` // (b, c, d, e, f)
-      header += `000000${NC} 0${padData(257-156)}ustar${NC}00${padData(512-264)}` // (g, h, i)
-      let data = `${contents}${padData((512 + contents.length) * 2)}`
-      tar.write(header + data)
-      tar.close()
-      tar = fs.createWriteStream(tarname, {start: 148, flags: 'r+'})
-      tar.write(calcChecksum(header))
-      tar.close()
-    } else {
-      console.warn('tarino: Error with filename ( >= 100 chars).')
-      throw e
+    if (Array.isArray(filename)) {
+      let i = 0
+      let entries = filename.map(function (fn) {
+        if (fn.length < 100) {
+          return {part: tarname.replace(/.tar$/, `.${++i}`), file: fn}
+        } else {
+          throw e
+        }
+      })
+      fs.closeSync(fs.openSync(tarname, 'w'))
+      writeTarEntries(tarname, entries)
+    }
+    else {
+      if (filename.length < 100) {
+        writeTarEntry(tarname, filename, function (header) {
+          writeChecksum(tarname, header, function () {
+            fs.appendFileSync(tarname, padData(PADDING))
+          })
+        })
+      } else {
+        throw e
+      }
     }
   }
   catch (e) {
-    console.log(e)
+    console.log(e.stack)
   }
 }
 
@@ -93,9 +150,8 @@ module.exports.createTarGz = function (tarnamegz, filename, options) {
     tar
     .pipe(zlib.createGzip())
     .pipe(gzip)
-  }
-  catch (e) {
-    console.warn(`tarino: Error creating ${tarnamgz}`)
+  } catch (e) {
+    console.warn(`tarino: Error creating ${tarnamegz}`)
     console.log(e)
   }
 }
