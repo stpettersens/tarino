0eaf4ba4b35b7a978b06e0863c3ee8724cb02137
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-12-30 20:15:36 +0000

    clean task

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@ const gulp = require('gulp')
 const mocha = require('gulp-mocha')
 const wait = require('gulp-wait')
 const sequence = require('gulp-sequence')
+const clean = require('gulp-rimraf')
 
 gulp.task('test1', function () {
   return gulp.src('tarino.test1.js')
@@ -13,6 +14,11 @@ gulp.task('test1', function () {
 gulp.task('test2', function () {
   return gulp.src('tarino.test2.js')
   .pipe(mocha({reporter: 'min', timeout: 100000}))
+})
+
+gulp.task('clean', function () {
+  return gulp.src('*.tar')
+  .pipe(clean())
 })
 
 gulp.task('test', sequence('test1', 'test2'))
